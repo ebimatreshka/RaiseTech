202620aa875c0569a7b7fb0ae17fa47d0eea3f2d
Emi <ebimatreshka3@gmail.com>
2022-08-15 23:07:57 +0900

    jQuery修正

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 //アコーディオンメニュー(効いた)
 $(function() {
     $('.header-hover').mouseover(function(){
-      $(this).children('.nav-menu1').stop().slideDown();
+      $(this).children('.nav-menu1').stop().slideDown('fast');
     });
     $('.header-hover').mouseout(function(){
-      $('.nav-menu1').stop().fadeOut();
+      $('.nav-menu1').stop().hide();
     });
   });
 
 
-//アコーディオンメニュー(×だった)  
-// $(function() {
-//     $('.header-hover').mouseover(function() {
-//         var $hover = $(this).find('.nav-menu1');
-//         if ($hover.hasClass('open')) {
-//           $hover.removeClass('open');
-//         } else {
-//           $hover.addClass('open');
-//         }
-//         });
-//     });
