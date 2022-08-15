1e5e2f2c7e70f9ebcd030beefcead2c657794239
Emi <ebimatreshka3@gmail.com>
2022-08-15 18:43:03 +0900

    調整

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+//アコーディオンメニュー(効いた)
 $(function() {
     $('.header-hover').mouseover(function(){
       $(this).children('.nav-menu1').stop().slideDown();
     });
     $('.header-hover').mouseout(function(){
-      $('.nav-menu1').stop().hide();
+      $('.nav-menu1').stop().fadeOut();
     });
   });
+
+
+//アコーディオンメニュー(×だった)  
+// $(function() {
+//     $('.header-hover').mouseover(function() {
+//         var $hover = $(this).find('.nav-menu1');
+//         if ($hover.hasClass('open')) {
+//           $hover.removeClass('open');
+//         } else {
+//           $hover.addClass('open');
+//         }
+//         });
+//     });
