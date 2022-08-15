c3347d66a38c97036d52fae6be55b317d9b95b50
Emi <ebimatreshka3@gmail.com>
2022-08-15 15:20:28 +0900

    アコーディオンメニュー

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.header-hover').mouseover(function(){
+      $(this).children('.nav-menu1').stop().slideDown();
+    });
+    $('.header-hover').mouseout(function(){
+      $('.nav-menu1').stop().hide();
+    });
+  });
