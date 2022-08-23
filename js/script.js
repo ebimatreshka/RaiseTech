735d99f0c190711993faed68b5ae60f417b65f52
Emi <ebimatreshka3@gmail.com>
2022-08-23 15:12:45 +0900

    spanの線なし×はあり

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ $(function() {
 		$(this).find('.nav-menu1').stop().hide();
 	});
 })
+
+//ハンバーガーボタン
+$('.hamburger-btn').on('click',function() {
+    $(this).toggleClass('is-open');
+    $('nav').toggleClass('is-open');
+    $('body').toggleClass('is-open');
+});
+
+// $(function() {
+//     $('.hamburger-btn').on('click',function() {
+//         if($(this).hasClass('active')) {
+//             $(this).removeClass('active');
+//             $('nav').removeClass('open')
+//         } else {
+//             $(this).addClass('active');
+//             $('nav').addClass('open');
+//         }
+//     });
+// });
