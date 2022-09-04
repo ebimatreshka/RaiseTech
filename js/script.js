e4b7810540a31aa8d5c02f0b150981a0f41fd1f4
Emi <ebimatreshka3@gmail.com>
2022-09-04 21:56:13 +0900

    完成その２

--- js/script.js
+++ js/script.js
@@ -25,3 +25,15 @@ $('.hamburger-btn-wrapper').on('click',function() {
 $('.header-hover').hover(function() {
     $(this).toggleClass('open');
 })
+
+//パララックス
+$(window).bind('scroll',function(e){
+    parallaxScroll();
+});
+function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('.main-img-layer1').css('background-position-y', ( 0 - (scrolled * 0.1)) + 'px');
+    $('.wrapper-inner1-center').css('top', ( 0 - (scrolled * .3) ) + 'px');
+};
+
+
